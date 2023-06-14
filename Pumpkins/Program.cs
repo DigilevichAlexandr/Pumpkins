8531604c219ab2b73c04ac23b0d470ff6aa92cb0
alexandr digilevich <digilevich.alexandr@gmail.com>
2023-06-14 19:56:37 +0300

    fix bug

--- Pumpkins/Program.cs
+++ Pumpkins/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Pumpkins
@@ -31,6 +30,7 @@ namespace Pumpkins
         {
             public Derection derection;
             public bool isFinishedDay;
+            public bool isAlive;
         }
 
         static void Main(string[] args)
@@ -40,26 +40,31 @@ namespace Pumpkins
             int p = int.Parse(inputs[0]);
             int d = int.Parse(inputs[1]);
             int n = int.Parse(inputs[2]);
+
+            if (d < 0 || d > 10 || n < 1 || n > 30 || p < 1 || p > n * n)
+                return;
+
             Root[,] grid = new Root[n, n];
             List<PumpkinInfo> pumpkins = new List<PumpkinInfo>();
 
             for (int i = 0; i < n; i++)
                 for (int j = 0; j < n; j++)
-                    grid[i, j] = new Root() { derection = Derection.None };
+                    grid[i, j] = new Root() { derection = Derection.None, isFinishedDay = true };
 
             for (int i = 0; i < p; i++)
             {
                 input = Console.ReadLine();
                 inputs = input.Split(' ');
                 pumpkins.Add(new PumpkinInfo() { row = int.Parse(inputs[0]), column = int.Parse(inputs[1]), isAlive = true });
-                grid[pumpkins[i].row, pumpkins[i].column] = new Root() { derection = Derection.Center };
-
+                grid[pumpkins[i].row, pumpkins[i].column] = new Root() { derection = Derection.Center, isAlive = true };
             }
 
             int day = 1;
 
             while (pumpkins.Where(pumpkin => pumpkin.isAlive).Count() > 0) //days
             {
+                RootsStartDay(grid, n);
+
                 for (int i = 0; i < p; i++)
                 {
                     if (pumpkins[i].isAlive)
@@ -70,22 +75,32 @@ namespace Pumpkins
                             grid[pumpkins[i].row - day, pumpkins[i].column].derection == Derection.None &&
                             //border from left or (no right direction from left, no center from left)
                             (pumpkins[i].column - 1 < 0 || (grid[pumpkins[i].row - day, pumpkins[i].column - 1].isFinishedDay || (
+                                !grid[pumpkins[i].row - day, pumpkins[i].column - 1].isAlive ||
+                                (grid[pumpkins[i].row - day, pumpkins[i].column - 1].isAlive &&
+                                !grid[pumpkins[i].row - day, pumpkins[i].column - 1].isFinishedDay &&
                                 grid[pumpkins[i].row - day, pumpkins[i].column - 1].derection != Derection.Right &&
-                                grid[pumpkins[i].row - day, pumpkins[i].column - 1].derection != Derection.Center))) &&
+                                grid[pumpkins[i].row - day, pumpkins[i].column - 1].derection != Derection.Center)))) &&
                             //border from up or (no down direction from up, no center from up)
                             (pumpkins[i].row - day - 1 < 0 || (grid[pumpkins[i].row - day - 1, pumpkins[i].column].isFinishedDay || (
+                                !grid[pumpkins[i].row - day - 1, pumpkins[i].column].isAlive ||
+                                (grid[pumpkins[i].row - day - 1, pumpkins[i].column].isAlive &&
+                                !grid[pumpkins[i].row - day - 1, pumpkins[i].column].isFinishedDay &&
                                 grid[pumpkins[i].row - day - 1, pumpkins[i].column].derection != Derection.Down &&
-                                grid[pumpkins[i].row - day - 1, pumpkins[i].column].derection != Derection.Center))) &&
+                                grid[pumpkins[i].row - day - 1, pumpkins[i].column].derection != Derection.Center)))) &&
                             //border from right or (no left direction from right, no center from right)
                             (pumpkins[i].column + 1 >= n || (grid[pumpkins[i].row - day, pumpkins[i].column + 1].isFinishedDay || (
-                                grid[pumpkins[i].row - day, pumpkins[i].column + 1].derection != Derection.Left
-                                && grid[pumpkins[i].row - day, pumpkins[i].column + 1].derection != Derection.Center))))
+                                !grid[pumpkins[i].row - day, pumpkins[i].column + 1].isAlive ||
+                                (grid[pumpkins[i].row - day, pumpkins[i].column + 1].isAlive &&
+                                !grid[pumpkins[i].row - day, pumpkins[i].column + 1].isFinishedDay &&
+                                grid[pumpkins[i].row - day, pumpkins[i].column + 1].derection != Derection.Left &&
+                                grid[pumpkins[i].row - day, pumpkins[i].column + 1].derection != Derection.Center)))))
                         {
                             grid[pumpkins[i].row - day, pumpkins[i].column].derection = Derection.Up;
+                            grid[pumpkins[i].row - day, pumpkins[i].column].isFinishedDay = true;
                         }
                         else
                         {
-                            Death(pumpkins, i, day);
+                            Death(pumpkins, i, day, grid, n);
                             continue;
                         }
 
@@ -94,46 +109,66 @@ namespace Pumpkins
                             grid[pumpkins[i].row, pumpkins[i].column + day].derection == Derection.None &&
                             //border from up or (no down direction from up, no center from up)
                             (pumpkins[i].row - 1 < 0 || (grid[pumpkins[i].row - 1, pumpkins[i].column + day].isFinishedDay || (
+                                !grid[pumpkins[i].row - 1, pumpkins[i].column + day].isAlive ||
+                                (grid[pumpkins[i].row - 1, pumpkins[i].column + day].isAlive &&
+                                !grid[pumpkins[i].row - 1, pumpkins[i].column + day].isFinishedDay &&
                                 grid[pumpkins[i].row - 1, pumpkins[i].column + day].derection != Derection.Down &&
-                                grid[pumpkins[i].row - 1, pumpkins[i].column + day].derection != Derection.Center))) &&
+                                grid[pumpkins[i].row - 1, pumpkins[i].column + day].derection != Derection.Center)))) &&
                             //border from right or (no left direction from left, no center from left)
                             (pumpkins[i].column + day + 1 >= n || (grid[pumpkins[i].row, pumpkins[i].column + day + 1].isFinishedDay || (
+                                !grid[pumpkins[i].row, pumpkins[i].column + day + 1].isAlive ||
+                                (grid[pumpkins[i].row, pumpkins[i].column + day + 1].isAlive &&
+                                !grid[pumpkins[i].row, pumpkins[i].column + day + 1].isFinishedDay &&
                                 grid[pumpkins[i].row, pumpkins[i].column + day + 1].derection != Derection.Left &&
-                                grid[pumpkins[i].row, pumpkins[i].column + day + 1].derection != Derection.Center))) &&
+                                grid[pumpkins[i].row, pumpkins[i].column + day + 1].derection != Derection.Center)))) &&
                             //border from down or (no up direction from down, no center from down)
                             (pumpkins[i].row + 1 >= n || (grid[pumpkins[i].row + 1, pumpkins[i].column + day].isFinishedDay || (
-                                grid[pumpkins[i].row + 1, pumpkins[i].column + day].derection != Derection.Left &&
-                                grid[pumpkins[i].row + 1, pumpkins[i].column + day].derection != Derection.Center))))
+                                !grid[pumpkins[i].row + 1, pumpkins[i].column + day].isAlive ||
+                                (grid[pumpkins[i].row + 1, pumpkins[i].column + day].isAlive &&
+                                !grid[pumpkins[i].row + 1, pumpkins[i].column + day].isFinishedDay &&
+                                grid[pumpkins[i].row + 1, pumpkins[i].column + day].derection != Derection.Up &&
+                                grid[pumpkins[i].row + 1, pumpkins[i].column + day].derection != Derection.Center)))))
                         {
                             grid[pumpkins[i].row, pumpkins[i].column + day].derection = Derection.Right;
+                            grid[pumpkins[i].row, pumpkins[i].column + day].isFinishedDay = true;
                         }
                         else
                         {
-                            Death(pumpkins, i, day);
+                            Death(pumpkins, i, day, grid, n);
                             continue;
                         }
 
-                        if (pumpkins[i].row + day < n &// down
-                                                       //is there empty place to grow
+                        if (pumpkins[i].row + day < n &&// down
+                                                        //is there empty place to grow
                             grid[pumpkins[i].row + day, pumpkins[i].column].derection == Derection.None &&
                             //border from right or (no left direction from right, no center from right)
                             (pumpkins[i].column + 1 >= n || (grid[pumpkins[i].row + day, pumpkins[i].column + 1].isFinishedDay || (
+                                !grid[pumpkins[i].row + day, pumpkins[i].column + 1].isAlive ||
+                                (grid[pumpkins[i].row + day, pumpkins[i].column + 1].isAlive &&
+                                !grid[pumpkins[i].row + day, pumpkins[i].column + 1].isFinishedDay &&
                                 grid[pumpkins[i].row + day, pumpkins[i].column + 1].derection != Derection.Left &&
-                                grid[pumpkins[i].row + day, pumpkins[i].column + 1].derection != Derection.Center))) &&
+                                grid[pumpkins[i].row + day, pumpkins[i].column + 1].derection != Derection.Center)))) &&
                             //border from down or (no up direction from down, no center from down)
                             (pumpkins[i].row + day + 1 >= n || (grid[pumpkins[i].row + day + 1, pumpkins[i].column].isFinishedDay || (
+                                !grid[pumpkins[i].row + day + 1, pumpkins[i].column].isAlive ||
+                                (grid[pumpkins[i].row + day + 1, pumpkins[i].column].isAlive &&
+                                !grid[pumpkins[i].row + day + 1, pumpkins[i].column].isFinishedDay &&
                                 grid[pumpkins[i].row + day + 1, pumpkins[i].column].derection != Derection.Up &&
-                                grid[pumpkins[i].row + day + 1, pumpkins[i].column].derection != Derection.Center))) &&
+                                grid[pumpkins[i].row + day + 1, pumpkins[i].column].derection != Derection.Center)))) &&
                             //border from left or (no right direction from left, no center from left)
                             (pumpkins[i].column - 1 < 0 || (grid[pumpkins[i].row + day, pumpkins[i].column - 1].isFinishedDay || (
+                                !grid[pumpkins[i].row + day, pumpkins[i].column - 1].isAlive ||
+                                (grid[pumpkins[i].row + day, pumpkins[i].column - 1].isAlive &&
+                                !grid[pumpkins[i].row + day, pumpkins[i].column - 1].isFinishedDay &&
                                 grid[pumpkins[i].row + day, pumpkins[i].column - 1].derection != Derection.Right &&
-                                grid[pumpkins[i].row + day, pumpkins[i].column - 1].derection != Derection.Center))))
+                                grid[pumpkins[i].row + day, pumpkins[i].column - 1].derection != Derection.Center)))))
                         {
                             grid[pumpkins[i].row + day, pumpkins[i].column].derection = Derection.Down;
+                            grid[pumpkins[i].row + day, pumpkins[i].column].isFinishedDay = true;
                         }
                         else
                         {
-                            Death(pumpkins, i, day);
+                            Death(pumpkins, i, day, grid, n);
                             continue;
                         }
 
@@ -142,22 +177,32 @@ namespace Pumpkins
                             grid[pumpkins[i].row, pumpkins[i].column - day].derection == Derection.None &&
                             //border from down or (no up direction from down, no center from down)
                             (pumpkins[i].row + 1 >= n || (grid[pumpkins[i].row + 1, pumpkins[i].column - day].isFinishedDay || (
+                                !grid[pumpkins[i].row + 1, pumpkins[i].column - day].isAlive ||
+                                (grid[pumpkins[i].row + 1, pumpkins[i].column - day].isAlive &&
+                                !grid[pumpkins[i].row + 1, pumpkins[i].column - day].isFinishedDay &&
                                 grid[pumpkins[i].row + 1, pumpkins[i].column - day].derection != Derection.Up &&
-                                grid[pumpkins[i].row + 1, pumpkins[i].column - day].derection != Derection.Center))) &&
+                                grid[pumpkins[i].row + 1, pumpkins[i].column - day].derection != Derection.Center)))) &&
                             //border from left or (no right direction from left, no center from left)
                             (pumpkins[i].column - day - 1 < 0 || (grid[pumpkins[i].row, pumpkins[i].column - day - 1].isFinishedDay || (
+                                !grid[pumpkins[i].row, pumpkins[i].column - day - 1].isAlive ||
+                                (grid[pumpkins[i].row, pumpkins[i].column - day - 1].isAlive &&
+                                !grid[pumpkins[i].row, pumpkins[i].column - day - 1].isFinishedDay &&
                                 grid[pumpkins[i].row, pumpkins[i].column - day - 1].derection != Derection.Right &&
-                                grid[pumpkins[i].row, pumpkins[i].column - day - 1].derection != Derection.Center))) &&
+                                grid[pumpkins[i].row, pumpkins[i].column - day - 1].derection != Derection.Center)))) &&
                             //border from up or (no down direction from up, no center from up)
                             (pumpkins[i].row - 1 < 0 || (grid[pumpkins[i].row - 1, pumpkins[i].column - day].isFinishedDay || (
+                                !grid[pumpkins[i].row - 1, pumpkins[i].column - day].isAlive ||
+                                (grid[pumpkins[i].row - 1, pumpkins[i].column - day].isAlive &&
+                                !grid[pumpkins[i].row - 1, pumpkins[i].column - day].isFinishedDay &&
                                 grid[pumpkins[i].row - 1, pumpkins[i].column - day].derection != Derection.Down &&
-                                grid[pumpkins[i].row - 1, pumpkins[i].column - day].derection != Derection.Center))))
+                                grid[pumpkins[i].row - 1, pumpkins[i].column - day].derection != Derection.Center)))))
                         {
                             grid[pumpkins[i].row, pumpkins[i].column - day].derection = Derection.Left;
+                            grid[pumpkins[i].row, pumpkins[i].column - day].isFinishedDay = true;
                         }
                         else
                         {
-                            Death(pumpkins, i, day);
+                            Death(pumpkins, i, day, grid, n);
                             continue;
                         }
 
@@ -165,7 +210,7 @@ namespace Pumpkins
                 }
 
                 //Print(grid, n);
-                RootsFinis(grid, n);
+                
                 day++;
             }
 
@@ -178,20 +223,32 @@ namespace Pumpkins
             Console.ReadLine();
         }
 
-        static void RootsFinis(Root[,] grid, int n)
+        static void RootsStartDay(Root[,] grid, int n)
         {
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
-                    if (grid[i, j].derection != Derection.None && grid[i, j].derection != Derection.Center)
-                        grid[i, j].isFinishedDay = true;
+                    if (grid[i, j].derection != Derection.None || grid[i, j].isAlive)
+                        grid[i, j].isFinishedDay = false;
             }
         }
 
-        static void Death(List<PumpkinInfo> pumpkins, int i, int day)
+        static void Death(List<PumpkinInfo> pumpkins, int i, int day, Root[,] grid, int n)
         {
             pumpkins[i].isAlive = false;
             pumpkins[i].dayDied = day;
+
+            for (int j = 1; j <= day; j++)
+            {
+                if (pumpkins[i].column + j < n && grid[pumpkins[i].row, pumpkins[i].column + j].derection == Derection.Right)
+                    grid[pumpkins[i].row, pumpkins[i].column + j].isAlive = false;
+                if (pumpkins[i].column - j >= 0 && grid[pumpkins[i].row, pumpkins[i].column - j].derection == Derection.Left)
+                    grid[pumpkins[i].row, pumpkins[i].column - j].isAlive = false;
+                if (pumpkins[i].row + j < n && grid[pumpkins[i].row + j, pumpkins[i].column].derection == Derection.Down)
+                    grid[pumpkins[i].row + j, pumpkins[i].column].isAlive = false;
+                if (pumpkins[i].row - j >= 0 && grid[pumpkins[i].row - j, pumpkins[i].column].derection == Derection.Up)
+                    grid[pumpkins[i].row - j, pumpkins[i].column].isAlive = false;
+            }
         }
 
         static void Print(Root[,] grid, int n)
